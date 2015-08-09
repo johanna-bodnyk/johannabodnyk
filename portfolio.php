0cb5b2289d457fda9fd0a352316bd5a5d369ced3
Johanna Bodnyk <bodnyk@gmail.com>
2015-08-09 19:59:30 -0400

    portfolio and resume updates

--- portfolio.php
+++ portfolio.php
@@ -1,9 +1,8 @@
 <?php include('_top.php'); ?>
     <main class="row" id="portfolio">
         <section class="row">
-            <h2>Freelance</h2>
             <section class="col-6">
-                <h3>The Circle School Meadow Campus Dream Campaign</h3>
+                <h2>Fundraising Website for The Circle School</h2>
                 <a href="http://meadowcampus.org">
                     <picture>                  
                         <source srcset="images/meadowcampus-588px.png" media="(min-width: 900px)">
@@ -12,49 +11,65 @@
                         <img srcset="images/meadowcampus-338px.png" alt="The Circle School Meadow Campus Dream Campaign.">
                     </picture>
                 </a>
+                <p>Full-stack freelance website project. Features include CSS animation and an admin interface for updating text content and adding donors.</p>
+                <p>
+                    <a href="http://meadowcampus.org">Live Site</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://github.com/johanna-bodnyk/meadowcampus">Code</a>
+                </p>
+                <p>Built with:</p>
                 <ul>
-                    <li>PHP (Laravel) &amp; MySQL</li>
-                    <li>Bootstrap</li>
+                    <li>Laravel 4 (PHP framework) &amp; MySQL</li>
+                    <li>CSS3 &amp; Bootstrap</li>
                     <li>JavaScript &amp; JQuery</li>
                 </ul>
             </section>
             <section class="col-6">
-                <h3>Trophy House</h3>
-                <a href="http://trophyhouseharrisburg.com">
-                    <picture>                  
-                        <source srcset="images/trophyhouse-588px.png" media="(min-width: 900px)">
-                        <source srcset="images/trophyhouse-338px.png" media="(min-width: 650px)">
-                        <source srcset="images/trophyhouse-588px.png" media="(min-width: 400px)">
-                        <img srcset="images/trophyhouse-338px.png" alt="The Trophy House.">
+                <h2>QuickList &mdash; A Simple JavaScript Task List</h2>
+                <a href="http://quicklist.higabba.biz/">
+                    <picture>
+                        <source srcset="images/quicklist-588px.png" media="(min-width: 900px)">
+                        <source srcset="images/quicklist-338px.png" media="(min-width: 650px)">
+                        <source srcset="images/quicklist-588px.png" media="(min-width: 400px)">
+                        <img srcset="images/quicklist-338px.png" alt="Quicklist JavaScript Task App.">
                     </picture>
                 </a>
+                <p>Final project for a JavaScript class. Uses localStorage for persistence (cloud storage feature coming soon&hellip;) </p>
+                <p>
+                    <a href="http://quicklist.higabba.biz">Demo</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://github.com/johanna-bodnyk/quicklist">Code</a>
+                </p>
+                <p>Built with:</p>
+                <ul>
+                    <li>JavaScript, JQuery, JQuery UI</li>
+                </ul>
             </section>
         </section>
         <section class="row">
-            <h2>Class Projects</h2>
             <section class="col-6">
-                <h3>Chirper</h3>
-                <picture>                  
-                    <source srcset="http://placehold.it/588x408" media="(min-width: 900px)">
-                    <source srcset="http://placehold.it/338x235" media="(min-width: 650px)">
-                    <source srcset="http://placehold.it/588x408" media="(min-width: 400px)">
-                    <img srcset="http://placehold.it/338x235" alt="Chirper.">
-                </picture>
+                <h2>Python XML Parser</h2>
+                <a href="https://github.com/johanna-bodnyk/sqlitetoxml">
+                    <picture>
+                        <source srcset="images/movietoxml-588px.png" media="(min-width: 900px)">
+                        <source srcset="images/movietoxml-338px.png" media="(min-width: 650px)">
+                        <source srcset="images/movietoxml-588px.png" media="(min-width: 400px)">
+                        <img srcset="images/movietoxml-338px.png" alt="The Circle School Meadow Campus Dream Campaign.">
+                    </picture>
+                </a>
+                <p>Python script to parse a SQLite database to XML files, created as an assignment for a database systems class.</p>
+                <p>
+                    <a href="https://github.com/johanna-bodnyk/sqlitetoxml">Code</a>
+                </p>
+                <p>Built with:</p>
                 <ul>
-                    <li>PHP (class framework) &amp; MySQL</li>
+                    <li>Python 3</li>
                 </ul>
             </section>
             <section class="col-6">
-                <h3>Cooking Calculator</h3>
-                <picture>                  
-                    <source srcset="http://placehold.it/588x408" media="(min-width: 900px)">
-                    <source srcset="http://placehold.it/338x235" media="(min-width: 650px)">
-                    <source srcset="http://placehold.it/588x408" media="(min-width: 400px)">
-                    <img srcset="http://placehold.it/338x235" alt="Cooking Calculator.">
-                </picture>
-                <ul>
-                    <li>JavaScript</li>
-                </ul>
+                <h2>"Tetris" Mobile Web App</h2>
+
+            </section>
+        </section>
+        <section class="row">
+            <section class="col-6">
+                <h2>Java Knitting Pattern Parser</h2>
             </section>
         </section>
     </main>
--- resume.php
+++ resume.php
@@ -3,18 +3,35 @@
         <div class="col-8">
             
             <h2>Experience</h2>
-            
+
+            <h3>Full-Stack Web Developer</h3>
+            <div class="row">
+                <p class="company">Freelance</p>
+                <p class="date">2014&ndash;present</p>
+            </div>
+            <p class="fl-subhead">Fundraising Website &mdash; The Circle School (launched February 2015):</p>
+            <ul>
+                <li>Solo developer for a PHP/MySQL-driven website (<a href="http://meadowcampus.org">meadowcampus.org</a>) to support the school's fundraising campaign to build a new campus.</li>
+                <li>Planned functionality in consultation with fundraising team, chose technologies (Laravel 4, Bootstrap, JQuery), developed, tested, and launched site.</li>
+                <li>Features include JavaScript pledge calculators, a CSS-animated campaign thermometer, and a custom admin interface allowing staff to post blog entries, add donor records, and update thermometer total.</li>
+            </ul>
+            <p class="fl-subhead">Website &amp; Online Course Platform &mdash; Women's Coding Collective (March 2015 &ndash; present):</p>
+            <ul>
+                <li>Working on a team with two other developers to migrate site (<a href="http://thewc.co">thewc.co</a>) from a custom PHP framework to Laravel 4.</li>
+                <li>Sections and features I have built to date include sign-up and authentication, password resetting, profile and avatar editing, referral discounts, JavaScript validation and messaging, and the admin interface.</li>
+                <li>Wrote script to convert and migrate avatar images for 5000+ users from current web server to Amazon S3 (asset storage solution for new site).</li>
+             </ul>
+
             <h3>Web Editor &amp; Communications Coordinator</h3>
             <div class="row">
                 <p class="company">Center for Middle Eastern Studies, Harvard University, Cambridge, MA</p>
-                <p class="date">Nov. 2009 – present</p>
+                <p class="date">2009&ndash;present</p>
             </div>
             <ul>
-                <li>Manage all aspects of Center website: work with third-party developers and Harvard technical support for upgrades and patches; monitor analytics; add new pages and features; solicit, write, and update content; and create documentation and train staff on administrative interface.</li>
-                <li>Develop and execute comprehensive communications strategy targeting internal and external audiences. Conceive and implement social media strategy including selecting platforms (currently Facebook, LinkedIn, and Vimeo) and developing moderation policies. Manage and moderate 2,100-member listserv.</li>
-                <li>Publicize Center events: develop series branding, design posters, pitch to Harvard Gazette and Crimson, coordinate videography, and advertise on website and via weekly email campaign.</li>
-                <li>Research and write stories and interviews highlighting Center projects and affiliate research.</li>
-                <li>Developing CRM solution for affiliate contact information: led discovery process, evaluated platform options, currently configuring Salesforce in preparation for data import.</li>
+                <li>Oversaw two successful migrations of the Center's 100+ page website (<a href="http://cmes.fas.harvard.edu">cmes.fas.harvard.edu</a>), in 2010 (from Drupal 5 to 6) and 2015 (from Drupal 6 to Harvard's Drupal 7&ndash;based OpenScholar platform).</li>
+                <li>Manage all aspects of website on ongoing basis: work with third-party developers and Harvard technical support for upgrades and patches; monitor analytics; add new pages and features; write and edit content; and create documentation and train staff on administrative interface.</li>
+                <li>Plan and execute comprehensive communications strategy covering website, email campaigns, social media, and print vehicles. Manage and moderate 2,100-member listserv. Publicize Center events, developing branding, designing posters, and coordinating photography and video.</li>
+                <li>Developing CRM solution for affiliate contact information: led discovery process, evaluated platform options, and configured Salesforce in preparation for data import.</li>
             </ul>
 
             <h3>Teaching Assistant, Dynamic Web Applications</h3>
@@ -23,22 +40,22 @@
                 <p class="date">Fall 2013, Summer &amp; Fall 2014</p>
             </div>
             <ul>
-                <li>Grade student projects and provide feedback and suggestions on code, interface, usability, and errors for dynamic web applications written in straight PHP, MySQL, and using Laravel.</li>
-                <li>Respond to posts in class forum to help troubleshoot student issues and answer questions regarding server and local development setup, Git, code syntax, and best practices.</li>
-                <li>Lead weekly section to help students troubleshoot issues in person and answer questions about and demonstrate examples of material covered in lecture.</li>
+                <li>Graded student projects and gave feedback on code, interface, usability, and errors for web applications written in both straight PHP/MySQL and using Laravel.</li>
+                <li>Responded to class forum posts to troubleshoot issues and answer questions regarding server and local development setup, version control, code syntax and logic, program design, and best practices.</li>
+                <li>Led weekly section to help troubleshoot project issues in real time and demonstrate lecture material.</li>
             </ul>
 
             <h3>Interactive Project Manager</h3>
             <div class="row">
                 <p class="company">Weymouth Design, Boston, MA</p>
-                <p class="date">Sep. 2006 – Sep. 2009</p>
+                <p class="date">2006&ndash;2009</p>
             </div>
             <ul>
-                <li>Managed web design projects from kick-off to delivery: built and maintained schedules and budgets, tracked progress and milestones, and served as the main point of contact between clients and project teams.</li>
-                <li>Coordinated internal team members and vendors including interactive designers, web developers, photographers, and video editors to deliver large-scale interactive projects for a variety of clients.</li>
+                <li>Managed large-scale interactive projects for a variety of clients from kick-off to delivery: built and maintained schedules and budgets, tracked progress and milestones, coordinated internal team and vendors, and served as the main point of contact between clients and project teams.</li>
                 <li>Led discovery process: identified business and technical requirements in consultation with clients, developers, and designers, and developed documentation including sitemaps, wireframes, and end-user guides.</li>
                 <li>Drafted and edited new business proposals and presentations; participated in project pitches, kick-offs, and other meetings with clients and account teams.</li>
             </ul>
+
         </div>
 
         <aside class="col-4">
@@ -47,16 +64,19 @@
             <h3>Harvard Extension School</h3>
             <div class="row">
                 <p class="degree">Certificate in Web Technologies</p>
-                <p class="date">Expected June 2015</p>
+                <p class="date">June 2015</p>
             </div>
-            <p>Coursework including:</p>
+            <p>Courses including:</p>
             <ul>
-                <li>CSCI E-22: Data Structures</li>
-                <li>CSCI E-34: User Experience Engineering</li>
-                <li>CSCI E-10B: Intro to Computer Science Using Java II</li>
-                <li>CSCI E-75: Dynamic Web Applications</li>
-                <li>CSCI S-1: Computer Science with Java</li>
-                <li>CSCI E-12: Fundamentals of Website Development</li>
+                <li>Database Systems</li>
+                <li>Data Structures</li>
+                <li>User Experience Engineering</li>
+                <li>Dynamic Web Applications</li>
+                <li>Fundamentals of Website Development</li>
+                <li>Web Programming Using JavaScript</li>
+                <li>Intro to Computer Science Using Java II</li>
+                <li>Intro to Computer Science with Java</li>
+                <li>Beyond the Basics of Frontend Web Design</li>
             </ul>
 
             <h3>Bard College</h3>
@@ -73,7 +93,7 @@
 
             <h2>Skills</h2>
             <ul>
-                <li><strong>Technologies and frameworks:</strong> PHP, Laravel, Drupal, Java, MySQL, HTML5, CSS3, Sass, Bootstrap, JavaScript, Jquery</li>
+                <li><strong>Languages and technologies:</strong> PHP, Laravel, Drupal, Java, Python, MySQL, HTML5, CSS3, Sass, Less, Bootstrap, JavaScript, JQuery, AWS, Git</li>
                 <li><strong>Graphic design software:</strong> Adobe Photoshop, InDesign, Illustrator, Bridge</li>
             </ul>
         </aside>
