5d9ed29d6dc5a45b524b36485ed015e223a51042
Johanna Bodnyk <bodnyk@gmail.com>
2015-08-25 21:34:27 -0400

    homepage text edits

--- home.php
+++ home.php
@@ -7,16 +7,19 @@
                         <source srcset="images/bodnyk.jpg" media="(min-width: 385px)">
                         <img srcset="images/bodnyk-landscape.jpg"  class="headshot">
                     </picture>
-                    I am a Boston-based web developer with strong PHP and JavaScript experience currently seeking a position as a full-stack or backend web developer.</p><p>Want to hire me to join your team? Check out my <a href="/resume">experience</a> and <a href="/portfolio">portfolio,</a> then <a href="/contact">say hi!</a></p>
+                    I am a Boston-based full-stack web&nbsp;developer with strong PHP and JavaScript skills and experience with modern web development tools and techniques. I particularly enjoy working with the PHP framework Laravel. In addition to PHP, I have experience programming in Java and Python.</p><p>Check out my <a href="/resume">experience</a> and <a href="/portfolio">portfolio,</a> then <a href="/contact">say hi!</a></p>
             </section>
             <aside class="col-4">
             <h2>At a glance&hellip;</h2>
                 <ul>
-                    <li>OO PHP, Java, Python</li>
-                    <li>MVC (Laravel)</li>
+                    <li>Object-oriented PHP</li>
+                    <li>Laravel</li>
+                    <li>Java</li>
+                    <li>Python</li>
                     <li>MySQL</li>
                     <li>JavaScript &amp; JQuery</li>
                     <li>HTML5/CSS3</li>
+                    <li>Mobile-responsive layouts</li>
                     <li>Sass, Less, Bootstrap</li>
                     <li>AWS</li>
                 </ul>
